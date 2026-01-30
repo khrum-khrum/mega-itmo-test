3526a9994f5597a7cf3f1b3a5d0cf4fb488a85d4
Kravchenko Yegor Alekseevich <yegkravchenko@ozon.ru>
2026-01-30 19:49:54 +0300

    fix: linter

--- test_main.py
+++ test_main.py
@@ -3,16 +3,19 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.text == "\"pong\""
+
 
 def test_echo():
     message = "hello"
     response = client.get(f"/echo/{message}")
     assert response.status_code == 200
     assert response.json() == message
+
 
 def test_health():
     response = client.get("/health")
